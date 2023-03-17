a0f0ab1e10dc572d95aaba5dfd6d6307712371e6
Medidave <davidawitor23@gmail.com>
2023-03-16 22:47:59 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with put function
+ * Return: 0.
+ */
+int main(void)
+{
+puts("\" Programming is like building a multilingual puzzle");
+return (0);
+};
+
