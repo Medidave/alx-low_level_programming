ae086ea29fdb169c7c3c235ccfcee698a261c10e
Medidave <davidawitor23@gmail.com>
2023-03-17 21:06:16 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single bit numbers from 0 to 9
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+}
+
