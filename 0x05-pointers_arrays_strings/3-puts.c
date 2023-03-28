a4e8895ebbb4076646478a381d7c99c40445dcae
Medidave <davidawitor23@gmail.com>
2023-03-27 21:18:59 -0700

    I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _puts - a function that prints a string, followed by a new line
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev -  a function that prints a string,
+ * in reverse, followed by a new line
+ * @s: The string to print
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c != 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
