215e526c758efcab86b1b58c712dca7427bf7b81
Medidave <davidawitor23@gmail.com>
2023-04-13 22:08:54 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int _putchar(char c);
 void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
