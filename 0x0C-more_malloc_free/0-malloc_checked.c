3bd2cfe85b86908a1d0e8ffffc712d151a72cb2d
Medidave <davidawitor23@gmail.com>
2023-04-13 20:51:49 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* malloc_checked - cause normal process termination with a status value of 98
+* @b: allocated memory
+* Return: a pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *david;
+
+	david = malloc(b);
+	if (david == NULL)
+		exit(98);
+	return (david);
+}
