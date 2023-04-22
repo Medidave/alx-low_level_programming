016c89dcfc528117c89ce0afbc5a3ac11532ba1a
Medidave <davidawitor23@gmail.com>
2023-04-21 22:22:31 -0700

    Beauty is variable, ugliness is constant

--- _putchar.c
+++ _putchar.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
- * @c:Ther character to print
+ * @c: the character to print 
  *
- * Return: On success 1.
- * On error, -1 is returned and errno is set appropriately.
+ * Return: On success 1
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
+
